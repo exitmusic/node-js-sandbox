6396309bd007fba1d09af83ca8a1567a55a62322
Kai Chang <kai.h.chang@gmail.com>
2012-05-24 11:03:45 -0500

    How not to implement request handler responses

--- request-handlers.js
+++ request-handlers.js
@@ -1,9 +1,25 @@
+var exec = require("child_process").exec;
+
 function start() {
 	console.log("Request handler 'start' was called.");
+	var content = "empty";
+	
+	exec("ls -lah", function (error, stdout, stderr) {
+		content = stdout;
+	});
+	
+	function sleep(milliSeconds) {
+		var startTime = new Date().getTime();
+		while (new Date().getTime() < startTime + milliSeconds);
+	}
+	sleep(10000);
+	
+	return content;
 }
 
 function upload() {
 	console.log("Request handler 'upload' was called.");
+	return "Hello Upload";
 }
 
 exports.start = start;
