99768eb9254cabbae8e5475c5fc239549783f09c
Kai Chang <kai.h.chang@gmail.com>
2012-05-24 11:49:18 -0500

    Implementing request handler responses the right way, short query

--- request-handlers.js
+++ request-handlers.js
@@ -4,7 +4,7 @@ function start(response) {
 	console.log("Request handler 'start' was called.");
 	var content = "empty";
 	
-	exec("find /", {timeout:10000, maxBuffer: 20000*1024}, function (error, stdout, stderr) {
+	exec("ls -lah", function (error, stdout, stderr) {
 		response.writeHead(200, {"Content-Type": "text/plain"});
 		response.write(stdout);
 		response.end();
