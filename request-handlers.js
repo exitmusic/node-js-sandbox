82d7f67745078bfecdef63d86009a617df1fa0e9
Kai Chang <kai.h.chang@gmail.com>
2012-05-24 13:33:58 -0500

    Fixed implementing request handler responses the right way.

--- request-handlers.js
+++ request-handlers.js
@@ -2,15 +2,12 @@ var exec = require("child_process").exec;
 
 function start(response) {
 	console.log("Request handler 'start' was called.");
-	var content = "empty";
 	
 	exec("ls -lah", function (error, stdout, stderr) {
 		response.writeHead(200, {"Content-Type": "text/plain"});
 		response.write(stdout);
 		response.end();
 	});
-	
-	return content;
 }
 
 function upload(response) {
