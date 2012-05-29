3b23009c2ded77037b20f7dff79f68fdf1f3fc24
Kai Chang <kai.h.chang@gmail.com>
2012-05-29 14:22:26 -0500

    Finished functionality to upload an image and display it in the web page.

--- request-handlers.js
+++ request-handlers.js
@@ -1,7 +1,8 @@
 var querystring = require("querystring");
 var fs = require("fs");
+var formidable = require("formidable");
 
-function start(response, postData) {
+function start(response) {
 	console.log("Request handler 'start' was called.");
 	
 	var body = '<!DOCTYPE html>'+
@@ -22,16 +23,32 @@ function start(response, postData) {
 	response.end();
 }
 
-function upload(response, postData) {
+function upload(response, request) {
 	console.log("Request handler 'upload' was called.");
-	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.write("You've sent: " + querystring.parse(postData).text);
-	response.end();
+	var form = new formidable.IncomingForm();
+	
+	console.log("about to parse");
+	form.parse(request, function(error, fields, files) {
+		console.log("parsing done");
+		console.log(files.upload.path);
+		
+		// Possible error on Windows systems: tried to rename to an already existing file
+		fs.rename(files.upload.path, "/tmp/test.png", function(err) {
+			if (err) {
+				fs.unlink("/tmp/test.png");
+				fs.rename(files.upload.path, "/tmp/test.png");
+			}
+		});
+		response.writeHead(200, {"Content-Type": "text/html"});
+		response.write('<p>received image:</p>');
+		response.write('<img src="/show"/>');
+		response.end();
+	});
 }
 
-function show(response, postData) {
+function show(response) {
 	console.log("Request handler 'show' was called.");
-	fs.readFile("/com/home/users/kchang/Downloads/top-100-small.png", "binary", function(error, file) {
+	fs.readFile("/tmp/test.png", "binary", function(error, file) {
 		if(error) {
 			response.writeHead(500, {"Content-Type": "text/plain"});
 			response.write(error + "\n");
