9ada3031cd1667d079251aa8d7caacf7582d5400
Kai Chang <kai.h.chang@gmail.com>
2012-05-31 17:14:59 -0500

    Added search route, passed sample filesystem output to the search view.

--- express-app/routes/index.js
+++ express-app/routes/index.js
@@ -1,12 +1,28 @@
+var fs = require("fs");
+var url = require("url");
+var query = require("querystring");
+var util = require("util");
+var exec = require("child_process").exec;
 
 /*
  * GET home page.
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' })
+  res.render('index', { title: 'Express' });
 };
 
 exports.about = function(req, res) {
-	res.render('about', {layout: false, title: 'About'})
+	res.render('about', {layout: false, title: 'About'});
+};
+
+exports.search = function(req, res) {
+	// search code here?
+	var query = url.parse(req.url).query;
+	var content = "empty";
+	
+	exec("ls -l", function(err, stdout, stderr) {
+		content = stdout;
+		res.render('search', {layout: false, query: content});
+	});
 };
