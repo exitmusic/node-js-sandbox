d3f5cd1574c4b18721a1853fbc7b1fb31d3b4b38
Kai Chang <kai.h.chang@gmail.com>
2012-06-11 17:58:02 -0500

    Turn search results into links.

--- express-app/app/models/search.js
+++ express-app/app/models/search.js
@@ -2,9 +2,13 @@ var exec = require('child_process').exec;
 
 exports.getResults = function (searchTerms, req, res) {
 	var content = "";
+	var filesArray;
 	
 	exec("ls -h ~ | grep -i " + searchTerms, function(err, stdout, stderr) {
-		content += stdout.split("\n");
+		filesArray = stdout.split("\n");
+		filesArray.forEach(function(element, index) {
+			content += '<a href="'+index+'">' + element + '</a>';
+		});
 		res.render('search', {query: content, title: 'Search', page: 'search'});
 	});
 }
