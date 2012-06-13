3eef7befc3acd5f39ed08b761ca889a4b8f5696a
Kai Chang <kai.h.chang@gmail.com>
2012-06-13 11:57:08 -0500

    Removed unused content object from the search model.

--- express-app/app/models/search.js
+++ express-app/app/models/search.js
@@ -2,23 +2,21 @@ var exec = require('child_process').exec;
 var result = require('./result');
 
 exports.getResults = function (searchTerms, req, res) {
-	var content = "<ul>";
 	var filesArray;
+	var searchPath;
 	var query = "ls -h ~ | grep -i " + searchTerms;
 	var results = [];
 	
 	
-	// TODO: Pass JSON object to view, not HTML content
+	// TODO: This can probably be replaced by existing functions in the File System module
 	exec(query, function(err, stdout, stderr) {
 		filesArray = stdout.split("\n");
 		filesArray.forEach(function(element, index) {
 			if (element !== "") {
 				var oneResult = new result(element+"-path", element+"-filename", element+"-filetype");
 				results.push(oneResult);
-				//content += '<li><a href="'+index+'">' + element + '</a></li>';
 			}
 		});
-		//content += "</ul>"
 		res.render('search', {results: results, title: 'Search'});
 	});
 }
