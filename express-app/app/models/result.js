80acc834ebaa95d05653b65c7e36a86bd518f75b
Kai Chang <kai.h.chang@gmail.com>
2012-06-13 11:26:49 -0500

    Created a result object to pass to the view instead of passing formatted HTML content.

---
+++ express-app/app/models/result.js
@@ -0,0 +1,7 @@
+function Result(path, filename, filetype) {
+		this.path = path;
+		this.filename = filename;
+		this.filetype = filetype;
+};
+
+module.exports = Result;
--- express-app/app/models/search.js
+++ express-app/app/models/search.js
@@ -1,17 +1,24 @@
 var exec = require('child_process').exec;
+var result = require('./result');
 
 exports.getResults = function (searchTerms, req, res) {
 	var content = "<ul>";
 	var filesArray;
+	var query = "ls -h ~ | grep -i " + searchTerms;
+	var results = [];
 	
-	exec("ls -h ~ | grep -i " + searchTerms, function(err, stdout, stderr) {
+	
+	// TODO: Pass JSON object to view, not HTML content
+	exec(query, function(err, stdout, stderr) {
 		filesArray = stdout.split("\n");
 		filesArray.forEach(function(element, index) {
 			if (element !== "") {
-				content += '<li><a href="'+index+'">' + element + '</a></li>';
+				var oneResult = new result(element+"-path", element+"-filename", element+"-filetype");
+				results.push(oneResult);
+				//content += '<li><a href="'+index+'">' + element + '</a></li>';
 			}
 		});
-		content += "</ul>"
-		res.render('search', {query: content, title: 'Search'});
+		//content += "</ul>"
+		res.render('search', {results: results, title: 'Search'});
 	});
 }
