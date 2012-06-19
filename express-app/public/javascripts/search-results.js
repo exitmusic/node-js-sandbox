73eaf13763aa131ad2253384fcdda6c072b3836a
Kai Chang <kai.h.chang@gmail.com>
2012-06-19 16:05:34 -0500

    Attach scroll event to window for compatibility on Chrome and FF. Added messaging when no results are found.

--- express-app/public/javascripts/search-results.js
+++ express-app/public/javascripts/search-results.js
@@ -9,13 +9,12 @@ $(document).ready(function() {
 	var refineSearchBoxWidth = $("#results-list").width()-32;
 	
 	$(window).resize(function() {
-		console.log($("#results-list").width());
 		refineSearchBoxWidth = $("#results-list").width()-32;
 		$("#refine-search").css("width", refineSearchBoxWidth+"px");
 	});
 	
 	$(window).scroll(function() {
-		if ($("body").scrollTop() > 53) {
+		if ($(window).scrollTop() > 53) {
 			$("#refine-search").addClass("fixed");
 			$("#refine-search").css("width", refineSearchBoxWidth+"px");
 		} else {
