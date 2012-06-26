6622f67ba50e664e7f50b1fcb56500f97de5c0df
Kai Chang <kai.h.chang@gmail.com>
2012-06-26 11:51:20 -0500

    Fixed search-results css path. Added search instructions.

--- express-app/app/controllers/search-controller.js
+++ express-app/app/controllers/search-controller.js
@@ -7,12 +7,10 @@ var auth = require('./../models/authentication');
 
 function routes(app) {
 	app.get('/search', auth.ensureAuthenticated, function(req, res) {
-		// search code here?
 		var query = url.parse(req.url, true).query;
 		var content = "";
 		
 		search.getResults(query.terms, req, res);
-		//search.getList('/com/home/users/kchang', req, res);
 	});
 }
 
