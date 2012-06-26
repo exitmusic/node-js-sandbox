289c89c3ce8666402d585d9d6aec5117c9034970
Kai Chang <kai.h.chang@gmail.com>
2012-06-26 14:50:48 -0500

    Separated parts of query into constant variables that are easier to read and modify.

--- express-app/app/models/search.js
+++ express-app/app/models/search.js
@@ -3,18 +3,24 @@ var fs = require('fs');
 var Result = require('./result');
 
 function getResults(searchTerms, req, res) {
-	var filesArray;
-	var searchPath = "~";
-	var query = "find " + searchPath + " -iname *" + searchTerms + "* -printf '%p %f wav\n'";
-	var results = [];
+	var SEARCH_PATH = "~";
+	var REG_EX_IGNORE_HIDDEN = "\\( ! -regex '.*/\\..*' \\)"; // Discards hidden files and directories (double escape backslashes)
+	var PRINT_FORMAT = "'%p||%f||wav\n'";
 	
+	var query = "find " + SEARCH_PATH + " " + REG_EX_IGNORE_HIDDEN + " -type f -iname '*" + searchTerms + "*' -printf " + PRINT_FORMAT;
+	var filesArray;
+	var results = [];
+
+	// TODO: Create a static file containing a directory listing the first time. Use grep to search.
+	//var queryOut = "find " + searchPath + " -printf '%p||%f||wav\n' > /tmp/query-find.log";
+	//exec (queryOut, function(err, stdout, stderr) {});
 	
 	// TODO: This can probably be replaced by existing functions in the File System module
 	exec(query, function(err, stdout, stderr) {
 		filesArray = stdout.split("\n");
 		filesArray.forEach(function(element, index) {
 			if (element !== "") {
-				elementParsed = element.split(" ");
+				elementParsed = element.split("||");
 				var oneResult = new Result(elementParsed[0], elementParsed[1], elementParsed[2]);
 				results.push(oneResult);
 			}
