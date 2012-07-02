c147c445c7b73bf72f3ecaf9aa82c7806492a1b8
Kai Chang <kai.h.chang@gmail.com>
2012-07-02 16:18:41 -0500

    Added underscore and async libraries. Allowed asynchronous file system queries, using async, when there are multiple search terms.

--- express-app/app/models/search.js
+++ express-app/app/models/search.js
@@ -1,27 +1,27 @@
 var exec = require('child_process').exec
   , fs = require('fs')
-  , stream = require('stream')
+  , async = require('async')
   , auth = require('./authentication')
   , campaign = require('./campaign')
   , Result = require('./result');
 
 function Search(searchTerms) {
-  this.searchTerms = searchTerms; //TODO: For now assume array positions
-  this.campaign = ""; //TODO: Call smart function to determine campaign from search terms
-  this.date = ""; //TODO: Call smart function to determine date from search terms
-  this.telephone = ""; //TODO: Call smart function to determine telephone from search terms
-  this.agent = ""; //TODO: Call smart function to determine agent from search terms
+  this.searchTerms = searchTerms; //TODO(kchang): For now assume array positions
+  this.campaign = ""; //TODO(kchang): Call smart function to determine campaign from search terms
+  this.date = ""; //TODO(kchang): Call smart function to determine date from search terms
+  this.telephone = ""; //TODO(kchang): Call smart function to determine telephone from search terms
+  this.agent = ""; //TODO(kchang): Call smart function to determine agent from search terms
 }
 /**
  * Gets results from the file system
  * @method getResults
- * @param {Array} searchTerms Array containing search terms // TODO: Remove this
+ * @param {Array} searchTerms Array containing search terms // TODO(kchang): Remove this
  * @param {http.ServerRequest} req Instance of Node's HTTP server request class
  * @param {http.ServerREsponse} res Instance of Node's HTTP server response class
  * @param {Function} cb Callback function to render the view
  */
 //function getResults(searchTerms, req, res, cb) {
-Search.prototype.getResults = function(req, res, cb) {
+Search.prototype.getResults = function(req, res, viewCallback) {
   var LOCAL_SEARCH_PATH = "~"
     , SEARCH_PATH = "/home"
     , REGEX_IGNORE_HIDDEN = "\\( ! -regex '.*/\\..*' \\)" // Discards hidden files and directories
@@ -41,40 +41,29 @@ Search.prototype.getResults = function(req, res, cb) {
   }) 
   
   // Search available directories for campaigns
-  //TODO: Code here
+  //TODO(kchang): Code here
   
   // Search agent, telephone, date
   searchTerms.forEach(function(searchTerm, index) {
     query = "find "
-      + validPaths[0] //TODO: Loop through all paths and create separate queries?
+      + validPaths[0] //TODO(kchang): Loop through all paths and create separate queries?
       + " "
       + REGEX_IGNORE_HIDDEN
       + " -type f -iname '*"
-      + searchTerm //TODO: Better solution
+      + searchTerm //TODO(kchang): Better solution
       + "*' -printf "
       + PRINT_FORMAT;
     queries.push(query);
   });
   
-  var numOfQueries = queries.length;
-  console.log(numOfQueries);
-  //queries.forEach(function(query, index) {
-
-      exec(queries[0], function(err, stdout, stderr) {
-        var filesArray
-          , elementParsed
-          , oneResult;
-        
-        filesArray = stdout.split("\n");
-        filesArray.forEach(function(element, index) {
-          if (element !== "") {
-            elementParsed = element.split("||"),
-            oneResult = new Result(elementParsed[0], elementParsed[1], elementParsed[2]);
-            results.push(oneResult);
-          }
-        });
-        //console.log(stdout);
-        exec(queries[1], function(err, stdout, stderr) {
+  /*async.parallel([
+    function(callback) {
+      queries.forEach(function(query) {
+        exec(query, function(err, stdout, stderr) {
+          var filesArray
+            , elementParsed
+            , oneResult;
+          
           filesArray = stdout.split("\n");
           filesArray.forEach(function(element, index) {
             if (element !== "") {
@@ -83,15 +72,39 @@ Search.prototype.getResults = function(req, res, cb) {
               results.push(oneResult);
             }
           });
-          cb(results, searchTerms, req, res); // calls the view
+          callback(null, results);
         });
+        
       });
+    }
+  ],
+  function(err, resultsx) {
+    console.log(results);
+    console.log(resultsx);
+    cb(resultsx, searchTerms, req, res);
+  });*/
+  
+  async.forEachSeries(queries, function(query, callback) {
+    exec(query, function(err, stdout, stderr) {
+      var filesArray
+        , elementParsed
+        , oneResult;
+      
+      filesArray = stdout.split("\n");
+      filesArray.forEach(function(element, index) {
+        console.log(2);
+        if (element !== "") {
+          elementParsed = element.split("||"),
+          oneResult = new Result(elementParsed[0], elementParsed[1], elementParsed[2]);
+          results.push(oneResult);
+        }
+      });
+      callback(null, results);
+    });
+  }, function(err) {
+    viewCallback(results, searchTerms, req, res); // calls the view
+  });
 
-  //});
-  
-
-  
-  
   // TODO: Testing solution, revisit this
   /*searchTerms.forEach(function(element, index) {
     query = "find " 
@@ -109,6 +122,7 @@ Search.prototype.getResults = function(req, res, cb) {
   //list = fs.readFileSync('/tmp/query-find.log');
   //campaigns = getCampaigns();
 };
+
 
 //TODO: Needed?
 function runQuery(query, cb) {
