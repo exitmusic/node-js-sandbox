01b3fb8aeb0ae01a75123e9f8a1df109a6ba27b0
Kai Chang <kai.h.chang@gmail.com>
2012-07-27 16:18:40 -0500

    Fixed filter box width on window resize. Improved search instructions.

--- express-app/public/js/search-results.js
+++ express-app/public/js/search-results.js
@@ -8,19 +8,17 @@ var resultsList = new List('results-list', options);
 $(document).ready(function() {
   var initialWindow = $(window)
     , filterSearch = $("#filter-wrap")
-    , filterSearchBoxWidth = $("#results-list").width()
     , results = $("#results");
   
   initialWindow.resize(function() {
-    filterSearchBoxWidth = $("#results-list").width();
-    filterSearch.css("width", filterSearchBoxWidth+"px");
+    filterSearch.css("width", $("#results-list").width()+"px");
   });
 
   initialWindow.scroll(function() {
     if ($(window).scrollTop() > 40) { // Need to get the latest window object
       filterSearch.addClass("fixed");
       results.addClass("fixed");
-      filterSearch.css("width", filterSearchBoxWidth+"px");
+      filterSearch.css("width", $("#results-list").width()+"px");
     } else {
       filterSearch.removeClass("fixed");
       results.removeClass("fixed");
